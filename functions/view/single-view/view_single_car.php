a9a3f98fed9f3d6589dde4f0ee11f6b652281537
= <s27153@pjwstk.edu.pl>
2023-06-12 19:53:05 +0200

    preparing single view for clients and car

---
+++ functions/view/single-view/view_single_car.php
@@ -0,0 +1,68 @@
+<html lang="pl">
+<head>
+    <title>Informacje o samochodzie</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container w-50 my-5 mx-auto">
+    <a class="btn btn-info mb-5" href="/index.html" type="button"><- Strona Główna</a>
+    <h1>Dane o samochodzie:
+        <?php
+        $id = $_GET["id"];
+        $mysql = new mysqli("localhost", "root", '', "wprg-project");
+        $stmt = $mysql->prepare("SELECT cars.CarID, cars.Model, cars.Production_year, cars.Mileage, cars.Engine_capacity, cars.Fuel_type, cars.Price, 
+       manufacturers.Manufacturer_name, categories.Name as Category_name FROM `cars` 
+                                LEFT JOIN manufacturers ON cars.Manufacturers_ManufacturerID = manufacturers.ManufacturerID
+                                LEFT JOIN categories ON cars.CategoryID = categories.CategoryID
+                                WHERE cars.CarID = ? ORDER BY cars.CarID;");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        echo $row["Manufacturer_name"] . " " . $row["Model"];
+        ?>
+    </h1>
+
+    <table class="table my-5">
+        <tbody>
+        <tr>
+            <td>ID samochodu:</td>
+            <td><?php echo($row["CarID"] ? $row["CarID"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Model samochodu:</td>
+            <td><?php echo($row["Model"] ? $row["Model"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Rok produkcji:</td>
+            <td><?php echo($row["Production_year"] ? $row["Production_year"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Przebieg:</td>
+            <td><?php echo($row["Mileage"] ? $row["Mileage"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Pojemność silnika:</td>
+            <td><?php echo($row["Engine_capacity"] ? $row["Engine_capacity"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Rodzaj silnika:</td>
+            <td><?php echo($row["Fuel_type"] ? $row["Fuel_type"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Cena: (zł)</td>
+            <td><?php echo($row["Price"] ? $row["Price"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Producent:</td>
+            <td><?php echo($row["Manufacturer_name"] ? $row["Manufacturer_name"] : "brak") ?></td>
+        </tr>
+        <tr>
+            <td>Kategorie:</td>
+            <td><?php echo($row["Category_name"] ? $row["Category_name"] : "brak") ?></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
---
+++ functions/view/single-view/view_single_client.php
@@ -0,0 +1,81 @@
+<html lang="pl">
+<head>
+    <title>Pojedyncza osoba</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container w-50 my-5 mx-auto">
+    <a class="btn btn-info mb-5" href="/index.html" type="button"><- Strona Główna</a>
+    <h1>Dane osoby:
+        <?php
+        $id = $_GET["id"];
+        $mysql = new mysqli("localhost", "root", '', "wprg-project");
+        $stmt = $mysql->prepare("SELECT clients.ClientID, clients.First_name, clients.Last_name, clients.Telephone_number, clients.Email, Addresses.AddressID, Addresses.City, 
+       Addresses.Street, Addresses.Street_number, Addresses.Post_code, offers.CarID FROM Clients 
+                        LEFT JOIN addresses ON clients.Addresses_AddressID = Addresses.AddressID
+                        JOIN offers ON clients.ClientID = offers.Clients_ClientID
+                        WHERE clients.ClientID = ?");
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        echo $row["First_name"] . " " . $row["Last_name"];
+        ?>
+    </h1>
+
+    <table class="table my-5">
+        <tbody>
+        <tr>
+            <td>Imię:</td>
+            <td><?php echo ($row["First_name"] ? $row["First_name"] : "brak")?></td>
+        </tr>
+        <tr>
+            <td>Nazwisko:</td>
+            <td><?php echo ($row["Last_name"] ? $row["Last_name"] : "brak")?></td>
+        </tr>
+        <tr>
+            <td>Pesel:</td>
+            <td><?php echo ($row["Telephone_number"] ? $row["Telephone_number"] : "brak")?></td>
+        </tr>
+        <tr>
+            <td>Data urodzenia:</td>
+            <td><?php echo ($row["Email"] ? $row["Email"] : "brak")?></td>
+        </tr>
+        <tr>
+            <td>Adres:</td>
+            <td><?php
+                $address = '';
+                if ($row["City"]) {
+                    $address .= $row["City"].', ';
+                }
+                if ($row["Street"]) {
+                    $address .= $row["Street"].', ';
+                }
+                if ($row["Street_number"]) {
+                    $address .= $row["Street_number"].', ';
+                }
+                if ($row["Post_code"]) {
+                    $address .= $row["Post_code"].', ';
+                }
+                echo ($address !== '') ? rtrim($address, ', ') : 'brak';
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <td>ID samochódów:</td>
+            <td>
+                <?php
+                $carID = isset($row["CarID"]) ? $row["CarID"] : "brak";
+                if ($carID !== "brak") {
+                    echo '<a href="view_single_car.php?id=' . $carID . '">' . $carID . '</a>';
+                } else {
+                    echo $carID;
+                }
+                ?>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
