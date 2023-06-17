8988979804398d038c6156b6060c201262dc0402
= <s27153@pjwstk.edu.pl>
2023-06-17 13:33:29 +0200

    changing login panel

--- functions/login.php
+++ functions/login.php
@@ -19,21 +19,40 @@ if (isset($_POST['zaloguj'])) {
             if ($user['Roles_RolesID'] == 1) {
                 header('Location: /functions/admin.php');
             } else if ($user['Roles_RolesID'] == 2) {
-                header('Location: /functions/mod.php');
+                header('Location: /functions/admin.php');
             } else if ($user['Roles_RolesID'] == 3) {
                 header('Location: /functions/admin.php');
             }
         } else {
-            echo "<h3>Nieprawidlowe haslo</h3>";
+            echo "<h3>Nieprawidłowe hasło</h3>";
         }
     } else {
-        echo "<h3>Nie znaleziono uzytkownika</h3>";
+        echo "<h3>Nie znaleziono użytkownika</h3>";
     }
 }
 ?>
-<h1>Login</h1>
-<form method="post">
-    <input type="text" name="login" placeholder="Login">
-    <input type="password" name="password" placeholder="Hasło">
-    <button type="submit" name="zaloguj">Zaloguj</button>
-</form>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+    <h1 class="mt-5">Login</h1>
+    <form method="post" class="mt-4">
+        <div class="mb-3">
+            <input type="text" name="login" class="form-control" placeholder="Login">
+        </div>
+        <div class="mb-3">
+            <input type="password" name="password" class="form-control" placeholder="Hasło">
+        </div>
+        <button type="submit" name="zaloguj" class="btn btn-primary">Zaloguj</button>
+    </form>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
